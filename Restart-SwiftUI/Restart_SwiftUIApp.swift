c0afb58fd35166e5e2c9ecf8ac5d374d5de6a578
abdulwariskhan <hellokhan.ch@gmail.com>
2024-02-23 23:12:37 +0100

    Initial Commit

---
+++ Restart-SwiftUI/Restart_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_SwiftUIApp.swift
+//  Restart-SwiftUI
+//
+//  Created by Khan on 23.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Restart_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
