1512101edb4b553c2209478a8322a9ccc699626d
abdulwariskhan <hellokhan.ch@gmail.com>
2024-02-23 23:49:40 +0100

    setup project

---
+++ Restart-SwiftUI/Screens/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Restart-SwiftUI
+//
+//  Created by Khan on 23.02.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ Restart-SwiftUI/Screens/OnBoardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingView.swift
+//  Restart-SwiftUI
+//
+//  Created by Khan on 23.02.2024.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoardingView()
+}
