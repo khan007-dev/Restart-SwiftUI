f03e9efeb69230d8c9080017a254feaf6e5a6250
abdulwariskhan <hellokhan.ch@gmail.com>
2024-02-24 23:03:15 +0100

    build Onboarding Screen

--- Restart-SwiftUI/Screens/OnBoardingView.swift
+++ Restart-SwiftUI/Screens/OnBoardingView.swift
@@ -16,16 +16,109 @@ struct OnBoardingView: View {
     // MARK: - BODY
     var body: some View {
         
-        VStack (spacing: 20) {
-            Text("Onboarding")
-                .font(.largeTitle)
+        ZStack {
             
-            Button(action: {
-                isOnboardingViewActive = false
-            }) {
-                Text("Start")
-            }
-        }
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            
+            VStack (spacing: 20) {
+               // MARK: - HEADER
+                
+                Spacer()
+                VStack (spacing: 0) {
+                    
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundStyle(.white)
+                    
+                    Text("""
+                            it is not how much we give but
+                            how much love we put into giving.
+                            """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundStyle(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                    
+                } // MARK: - HEADER
+                
+                // MARK: - CENTER
+                
+                ZStack {
+                    
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    } // : ZSTACK
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                } //: CENTER
+                
+                // MARK: - FOOTER
+                
+                ZStack {
+                    
+                    // PART OF THE CUSTOM BUTTON
+                    
+                    // 1. BACKGROUND (STATIC)
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    
+                    // 2. CALL-TO-ACTION (STATIC)
+                    
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .foregroundStyle(.white)
+                        .offset(x: 20)
+                    // 3. CAPSULE (DUNAMIC WIDTH)
+                    
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        Spacer()
+                    }
+                    // 4. CIRLCE (DRAGGABLE)
+                    
+                    HStack {
+                        ZStack {
+                         
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                            
+                        }
+                        .foregroundColor(.white)
+                    .frame(width: 80, height: 80, alignment: .center)
+                    .onTapGesture {
+                        isOnboardingViewActive = false
+                    }
+                        Spacer()
+                    }// : HSTACK
+                } //: ZSTACK
+                .frame(height: 80, alignment: .center)
+                .padding()
+                
+            } // : VSTACK
+        } // : ZSTACK
         
     }
 }
