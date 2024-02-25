e7e036aebb4982a90de8b7c0385ecd8d246aeace
abdulwariskhan <hellokhan.ch@gmail.com>
2024-02-25 13:08:54 +0100

    Developed Home Screen with SwiftUI

---
+++ Restart-SwiftUI/Views/CircleGroupView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleGroupView.swift
+//  Restart-SwiftUI
+//
+//  Created by Khan on 25.02.2024.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleGroupView()
+}
